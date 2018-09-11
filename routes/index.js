2ef88acf7a7ae65eab18cea0554915db0c674d0d
Yongzhe Deng <whizkiddeng@gmail.com>
2018-09-11 16:42:54 +0800

    add rules for points

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var points = require('../models/points');
+var config = require('../configs/global/config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   points.get(1, function (err, data) {
   	console.log(data);
+  	console.log(config);
   	res.render('index', { title: 'Express' });
   });
 });
