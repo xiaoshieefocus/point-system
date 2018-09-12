9d23e81f8338d7fa53337f5b30cc6c9a79265b0a
mzss0 <652352131@qq.com>
2018-09-12 22:00:29 +0800

    fix
    fix

--- models/pointsLogsBreakdown.js
+++ models/pointsLogsBreakdown.js
@@ -4,6 +4,24 @@ var db = require('../lib/db'),
     config = require('../configs/global/config'),
     pointsLogsBreakdown = {};
 
+function changeTwoDecimal_f(x) {
+    var f_x = parseFloat(x);
+    if (isNaN(f_x)) {
+        return false;
+    }
+    var f_x = Math.round(x * 100) / 100;
+    var s_x = f_x.toString();
+    var pos_decimal = s_x.indexOf('.');
+    if (pos_decimal < 0) {
+        pos_decimal = s_x.length;
+        s_x += '.';
+    }
+    while (s_x.length <= pos_decimal + 2) {
+        s_x += '0';
+    }
+    return s_x;
+}
+
 pointsLogsBreakdown.getDistributor = function (condition, callback) {
     var params = [],
         q = '';
@@ -15,10 +33,26 @@ pointsLogsBreakdown.getDistributor = function (condition, callback) {
         params.push(condition.user);
     }
     db.executeQuery(q, params, function (err, data){
+        var distributors = [],
+            purchase_money_saved = [];
         data.forEach(function(item){
-            ;
+            if(distributors.indexOf(item.distributor_name) < 0){
+                distributors.push(item.distributor_name);
+                purchase_money_saved.push(parseFloat(item.purchase_money_saved));
+            } else {
+                purchase_money_saved[distributors.indexOf(item.distributor_name)] += parseFloat(item.purchase_money_saved);
+            }
+
         });
-        callback(err, data);
+        var result = [];
+        for (var i = 0; i < purchase_money_saved.length; i++){
+            var item = {
+                distributor: distributors[i],
+                purchase_money_saved: changeTwoDecimal_f(purchase_money_saved[i])
+            }
+            result.push(item);
+        }
+        callback(err, result);
     });
 };
 
